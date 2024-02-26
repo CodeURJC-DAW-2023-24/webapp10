1635f5fbb8d3c727f38adb79660c9497f9718805
Vicente Navarro <vicente6125@gmail.com>
2024-02-26 17:38:46 +0100

    errors fixed

--- src/main/java/es/codeurj/mortez365/controller/AppController.java
+++ src/main/java/es/codeurj/mortez365/controller/AppController.java
@@ -3,13 +3,13 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorController;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,10 +30,7 @@ public class AppController {
         return "index";
     }
 
-   
 
-
-  
     @GetMapping("/roulette")
     public String roulette() {
         return "roulette";
--- src/main/java/es/codeurj/mortez365/model/Wallet.java
+++ src/main/java/es/codeurj/mortez365/model/Wallet.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
+
 
 @Entity
 @Data
