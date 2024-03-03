d33c6c0dcd117f1f4b9331119c0565a26f83fc36
Vicente Navarro <vicente6125@gmail.com>
2024-03-03 13:26:34 +0100

    logout created

--- src/main/java/es/codeurj/mortez365/initializer/DatabaseUserLoader.java
+++ src/main/java/es/codeurj/mortez365/initializer/DatabaseUserLoader.java
@@ -14,19 +14,5 @@ import java.util.List;
 @Component
 public class DatabaseUserLoader {
 
-    @Autowired
-    private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @PostConstruct
-    private void initDatabase() {
-
-        userRepository.save(new User("Alaro", "Pindado", "Castiñeira",
-                "emailfalso@gmail.com", Date.from(Instant.now()),"Español","123456789V",
-                "user", passwordEncoder.encode("pass"),false,"Tu mama",
-                "12345","123456789", List.of("USER")));
-
-    }
 }
