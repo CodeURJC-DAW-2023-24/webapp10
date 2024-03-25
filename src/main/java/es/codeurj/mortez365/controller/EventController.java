2aab204b2bff4319171c3a68a7f12c11f0287e30
Vicente Navarro <vicente6125@gmail.com>
2024-03-25 14:03:24 +0100

    API methods for events

--- src/main/java/es/codeurj/mortez365/controller/EventController.java
+++ src/main/java/es/codeurj/mortez365/controller/EventController.java
@@ -1,16 +1,27 @@
 package es.codeurj.mortez365.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
+
+
+
+import org.springframework.http.ResponseEntity;
 
 import es.codeurj.mortez365.model.Event;
 import es.codeurj.mortez365.repository.EventRepository;
+import es.codeurj.mortez365.service.EventSevice;
 import jakarta.annotation.PostConstruct;
 
 @RestController
@@ -18,6 +29,9 @@ import jakarta.annotation.PostConstruct;
 public class EventController {
     @Autowired
     private EventRepository events;
+
+    @Autowired
+    private EventSevice eventService;
 
      //Initialize events
     @PostConstruct
@@ -43,13 +57,40 @@ public class EventController {
     
     }
 
-    @GetMapping("/events")
+    @GetMapping("/")
     public List<Event> getAllevents() {
         return events.findAll();
     }
- 
-    
 
+    @PostMapping("/")
+    public ResponseEntity<Event> newEvent(@RequestBody Event newEvent) {
+       eventService.save(newEvent);
+        URI location = fromCurrentRequest().path("/{id}").buildAndExpand(newEvent.getId()).toUri();
+        return ResponseEntity.created(location).body(newEvent);
+    }
+ 
+    @GetMapping("/events/{id}")
+    public ResponseEntity<Optional<Event>> getEventById(@PathVariable Long id) {
+        Optional<Event> event = events.findById(id);
+        
+        if (event == null) {
+           return ResponseEntity.ok(event);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Event> deleteEvent(@PathVariable Long id) {
+       Event event = events.findById(id).orElse(null);
+        if (event == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            events.deleteById(id);
+            return ResponseEntity.ok(event);
+        }
+    }
     
 }
 
