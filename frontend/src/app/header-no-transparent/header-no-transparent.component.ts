d3c94245b2a75238c1fce44c68b10f22fefde7ce
josrob13 <ja.villagran.2021@alumnos.urjc.es>
2024-05-01 20:50:09 +0200

    fixed header register

--- frontend/src/app/header-no-transparent/header-no-transparent.component.ts
+++ frontend/src/app/header-no-transparent/header-no-transparent.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-no-transparent',
   templateUrl: './header-no-transparent.component.html',
-  styleUrl: './header-no-transparent.component.css'
+  styleUrls: ['../../styles.css', 'header-no-transparent.component.css']
 })
 export class HeaderNoTransparentComponent {
 
