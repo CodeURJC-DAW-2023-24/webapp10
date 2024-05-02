b039871aa42eb0e4db562f3430b9da3988f48e61
Guillalox <guillalox@gmail.com>
2024-05-02 18:16:25 +0200

    choose what to bet for

--- frontend/src/app/singlebet/singlebet.component.ts
+++ frontend/src/app/singlebet/singlebet.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { EventsService } from '../services/events.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -9,17 +9,43 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class SinglebetComponent {
   event: any;
+  feeT:number = 0;
+  feeL:number = 0;
+
+  @ViewChild('selectedBetInput') selectedBetInput!: ElementRef;
 
   constructor(private router: Router,activatedRoute: ActivatedRoute, private eventsService: EventsService) {
-
     let id = activatedRoute.snapshot.params['id'];
-    eventsService.getEventById(id).subscribe( 
-      event => this.event = event,
+
+    eventsService.getEventById(id).subscribe(
+      event => {
+        this.event = event;
+        this.feeT = 1.7;
+        this.feeL = +(3.5 - event.fee).toFixed(2);
+      },
       error => console.error(error)
     );
 
   }
-   }
 
- 
+  selectBet(betType: string, event: Event) {
+    console.log("LLEGA");
+    const betButtons = document.querySelectorAll('.bordered-btn-bets');
+
+    // Deseleccionar todos los botones
+    betButtons.forEach(button => {
+      button.classList.remove('selected');
+    });
+
+    // Seleccionar el botón actual
+    const target = event.target as HTMLElement;
+    target.classList.add('selected');
+
+    // Asignar el tipo de apuesta al input
+    this.selectedBetInput.nativeElement.value = betType;
+  }
+
+}
+
+
 
