2ec0dd3d8f2a5a928082d474e0432d726cc77ed1
Alvaropin17 <alvaro17pc@gmail.com>
2024-05-04 19:59:14 +0200

    comment component

--- frontend/src/app/comment/comment.component.ts
+++ frontend/src/app/comment/comment.component.ts
@@ -11,4 +11,42 @@ import { CommentService } from '../services/comment.service';
 })
 export class CommentComponent {
 
+  constructor(private commentService: CommentService) { }
+
+  addComment(id: number, commentText: string): void {
+    this.commentService.addComment(id, commentText)
+      .subscribe(
+        response => {
+          console.log('Comentario agregado:', response);
+        },
+        error => {
+          console.error('Error al agregar comentario:', error);
+        }
+      );
+  }
+
+  editComment(eventId: number, id: number, newComment: string): void {
+    this.commentService.editComment(eventId, id, newComment)
+      .subscribe(
+        response => {
+          console.log('Comentario editado:', response);
+
+        },
+        error => {
+          console.error('Error al editar comentario:', error);
+        }
+      );
+  }
+
+  deleteComment(eventId: number, id: number): void {
+    this.commentService.deleteComment(eventId, id)
+      .subscribe(
+        response => {
+          console.log('Comentario eliminado:', response);
+        },
+        error => {
+          console.error('Error al eliminar comentario:', error);
+        }
+      );
+  }
 }
